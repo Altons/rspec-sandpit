ad3bc7e615796faba070eed5ffa0163a1f171844
Alberto Negron <albertonegron@gmail.com>
2020-01-28 18:08:44 +0000

    explicit subject

---
+++ spec/explicit_subject_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Hash do
+  subject(:bob) do
+    { a: 1, b: 2 }
+  end
+  it 'has two key-value pairs' do
+    expect(subject.length).to eq(2)
+    expect(bob.length).to eq(2)
+  end
+end
