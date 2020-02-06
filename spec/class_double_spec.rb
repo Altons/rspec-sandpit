f3e9c457b8e1b7d2acdb36fbc6c2f22703135cdd
Alberto Negron <albertonegron@gmail.com>
2020-02-06 13:01:00 +0000

    class doubles

---
+++ spec/class_double_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class Deck
+  def self.build
+    # business logic to build a whole bunch of cards
+  end
+end
+class CardGame
+  attr_reader :cards
+  def start
+    @cards = Deck.build
+  end
+end
+
+RSpec.describe CardGame do
+  it 'can only implement class methods that are defined on a class' do
+    # class_double('Deck', build: ['Ace', 'Queen'])
+    # class_double(Deck, build: ['Ace', 'Queen'])
+    deck_klass = class_double(Deck, build: %w[Ace Queen]).as_stubbed_const
+    expect(deck_klass).to receive(:build)
+    subject.start
+    expect(subject.cards).to eq(%w[Ace Queen])
+  end
+end
